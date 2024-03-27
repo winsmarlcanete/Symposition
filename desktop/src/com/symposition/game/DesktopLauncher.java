be46c94e1de71ef1e28b9d1ea55da983505997bc
Winsmarl <110825438+winsmarlcanete@users.noreply.github.com>
2024-03-27 17:04:25 +0800

    Initial commit

---
+++ desktop/src/com/symposition/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.symposition.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.symposition.game.Symposition;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Symposition");
+		new Lwjgl3Application(new Symposition(), config);
+	}
+}
